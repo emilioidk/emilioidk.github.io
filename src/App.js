e155e870c2cea01f2dbb7ef8e5805ac3af8f7044
Emilio Martin Lundgaard Lopez <emilio@lundgaardlopez.com>
2018-08-18 23:30:27 +0200

    Remove create-react-app placeholder code; Implement routing skeleton

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import logo from './logo.svg';
+import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-  /**
-   * Function for performing all the behind the scenes between MaterialUI and ReactRouter to make a kickass SPA
-   * @param {object} event
-   * @param {number} value
-   */
-  onTabChange(event, value) {
-    console.log("Fix me!");
-  }
-
   render() {
     return (
       <div className="App">
-        <AppBar>
-          <Tabs fullWidth={true} centered={true} onChange={this.onTabChange}>
-            <Tab label="Home" />
-            <Tab label="Blog" />
-            <Tab label="Projects" />
-            <Tab label="Contact" />
-          </Tabs>
-        </AppBar>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <AppBar>
+            <Tabs fullWidth={true} centered={true}>
+              <Tab value="home" component={Link} to="/" label="Home" />
+              <Tab value="blog" component={Link} to="/blog" label="Blog" />
+              <Tab value="projects" component={Link} to="/projects" label="Projects" />
+              <Tab value="contact" component={Link} to="/contact" label="Contact" />
+            </Tabs>
+          </AppBar>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Button variant="contained" color="primary">It works!</Button>
+        <section style={{ padding: 8 * 8 }}>
+          <Switch>
+            <Route exact path="/"><div>Home</div></Route>
+            <Route exact path="/blog"><div>Blog</div></Route>
+            <Route exact path="/projects"><div>Projects</div></Route>
+            <Route exact path="/contact"><div>Contact</div></Route>
+          </Switch>
+        </section>
       </div>
     );
   }
